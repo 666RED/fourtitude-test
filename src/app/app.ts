737c8208a1b525af3f0b43bd2a71dd757b865bb8
SEE HONG CHEN <hongchensee8@gmail.com>
2025-09-16 21:51:10 +0800

    remove router outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, HostListener, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterOutlet } from '@angular/router';
 import { Landing } from '@src/app/features/main-page/landing/landing';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Landing, MatIconModule],
+  imports: [Landing, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
